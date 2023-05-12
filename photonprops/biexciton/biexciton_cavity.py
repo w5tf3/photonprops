789705c4f10f18cb6650e3de6b7279c584537fdf
w5tf3 <48763345+w5tf3@users.noreply.github.com>
2023-05-12 13:24:39 +0200

    add pol. entanglement with and without cavity

--- photonprops/biexciton/biexciton_cavity.py
+++ photonprops/biexciton/biexciton_cavity.py
@@ -1,6 +1,7 @@
 import numpy as np
 import qutip as qt
 from photonprops.commons.pulse import ChirpedPulse
+from concurrent.futures import ThreadPoolExecutor, wait
 
 HBAR = 0.6582173  # meV*ps
 
@@ -11,16 +12,16 @@ def energies(delta_b=4., delta_0=0.):
     E_B = -delta_b
     return E_X, E_Y, E_B
 
-def g2_b(ops,options):
+def g2_b_cav(ops,options):
     a_op,b_op,c_op = ops.split(",")
     options["a_op"] = a_op
     options["b_op"] = b_op
     options["c_op"] = c_op
-    options["mode"] = "entanglement"
-    g2,_,_ = biexciton_system(**options)
+    options["mode"] = "g2"
+    g2,_,_ = biexciton_system_cav(**options)
     return g2
 
-def biexciton_system(tau1=1, tau2=1, area1=1*np.pi, area2=0, det1=0, det2=0, alpha1=0, alpha2=0, pol1_x=1, pol2_x=1, t01=10, t02=10, tend=0, delta_b=4, delta_0=0.0, gamma_e=1/100, gamma_b=2/100, dt_1=0.1, dt_2=1, options=qt.Options(atol=1e-7), mode="pop", g2_pol="x",a_op="ax.dag()",b_op="ax.dag()*ax",c_op="ax", abs=False, exp_t=False):
+def biexciton_system_cav(tau1=1, tau2=1, area1=1*np.pi, area2=0, det1=0, det2=0, g_cav=0.25, kappa=1.0, alpha1=0, alpha2=0, pol1_x=1, pol2_x=1, t01=10, t02=10, delta_b=4, delta_0=0.0, gamma_e=1/100, gamma_b=2/100, epsilon=0.01, dt_1=0.1, dt_2=1, tend=100, n_phot=1, options=qt.Options(atol=1e-7), mode="pop", exp_t=False,a_op="ax.dag()",b_op="ax.dag()*ax",c_op="ax", abs=False, initial_state="g"):
     """
     in qutip, every energy has to be provided in 1/ps
     Here, a rotating frame with the unsplit exciton energy is chosen. 
@@ -45,42 +46,53 @@ def biexciton_system(tau1=1, tau2=1, area1=1*np.pi, area2=0, det1=0, det2=0, alp
     gamma_b = gamma_b / 2  # both X and Y decay. the input gamma_b is 1/tau_b, where tau_b is the lifetime of the biexciton
 
     # system states
-    g = qt.basis(4,0)
-    x = qt.basis(4,1)
-    y = qt.basis(4,2)
-    b = qt.basis(4,3)
+    g = qt.tensor(qt.basis(4,0),qt.basis(n_phot+1,0),qt.basis(n_phot+1,0))
+    x = qt.tensor(qt.basis(4,1),qt.basis(n_phot+1,0),qt.basis(n_phot+1,0))
+    y = qt.tensor(qt.basis(4,2),qt.basis(n_phot+1,0),qt.basis(n_phot+1,0))
+    b = qt.tensor(qt.basis(4,3),qt.basis(n_phot+1,0),qt.basis(n_phot+1,0))
     # print(g*g.dag())  # should be 4x4 Matrix with 1.0 at (0,0)
+    ax = qt.tensor(qt.qeye(4),qt.destroy(n_phot+1),qt.qeye(n_phot+1))
+    ay = qt.tensor(qt.qeye(4),qt.qeye(n_phot+1),qt.destroy(n_phot+1))
 
+    n_cavx = ax.dag() * ax
+    n_cavy = ay.dag() * ay
+    # operators for number/polarization ops. containing all photon states
+    _g = qt.tensor(qt.basis(4,0),qt.qeye(n_phot+1),qt.qeye(n_phot+1))
+    _x = qt.tensor(qt.basis(4,1),qt.qeye(n_phot+1),qt.qeye(n_phot+1))
+    _y = qt.tensor(qt.basis(4,2),qt.qeye(n_phot+1),qt.qeye(n_phot+1))
+    _b = qt.tensor(qt.basis(4,3),qt.qeye(n_phot+1),qt.qeye(n_phot+1))
     # number operators
-    n_x = x * x.dag()
-    n_y = y * y.dag()
-    n_b = b * b.dag()
+    n_g = _g * _g.dag()
+    n_x = _x * _x.dag()
+    n_y = _y * _y.dag()
+    n_b = _b * _b.dag()
 
     # transition operators / polarizations
-    p_gx = g * x.dag()
-    p_gy = g * y.dag()
-    p_xb = x * b.dag()
-    p_yb = y * b.dag()
+    p_gx = _g * _x.dag()
+    p_gy = _g * _y.dag()
+    p_xb = _x * _b.dag()
+    p_yb = _y * _b.dag()
     # this one is not needed for the hamiltonian
-    p_gb = g * b.dag()
+    p_gb = _g * _b.dag()
 
-    ax = p_gx + p_xb
-    ay = p_gy + p_yb
+    initial = eval(initial_state)
 
     # collapse operators / spontaneous emission
-    c_ops = [np.sqrt(gamma_e) * p_gx, np.sqrt(gamma_e) * p_gy, np.sqrt(gamma_b) * p_xb, np.sqrt(gamma_b) * p_yb]
+    # take care if (ax + ay) or separate
+    c_ops = [np.sqrt(kappa) * ax, np.sqrt(kappa) * ay,np.sqrt(gamma_e) * p_gx, np.sqrt(gamma_e) * p_gy, np.sqrt(gamma_e) * p_xb, np.sqrt(gamma_e) * p_yb]
+    # c_ops = [np.sqrt(gamma_e) * p_gx, np.sqrt(gamma_e) * p_gy, np.sqrt(gamma_b) * p_xb, np.sqrt(gamma_b) * p_yb]
 
     # system Hamiltonian
     E_X, E_Y, E_B = energies(delta_b=delta_b, delta_0=delta_0) # note they are already divided by HBAR
-    H_sys = E_X * n_x + E_Y * n_y + E_B * n_b
+    H_sys = E_X * n_x + E_Y * n_y + E_B * n_b + E_B/2 * (n_cavx + n_cavy)
 
     # pulse 1 and 2, right now assume delay > 0
     tau11=np.sqrt(alpha1**2 / tau1**2 + tau1**2)
     tau22=np.sqrt(alpha2**2 / tau2**2 + tau2**2)
     # choose the longer of the two
-    t_start1 = t01 # 4*tau11 if tau11 > tau22 else 4*tau22
+    t_start1 = t01 #4*tau11 if tau11 > tau22 else 4*tau22
     # further delay pulse 2
-    t_start2 = t02 # t_start1 + delay
+    t_start2 = t02  # t_start1 + delay
     pulse1 = ChirpedPulse(tau1, det1, alpha1, t0=t_start1, e0=area1, polar_x=pol1_x)
     pulse2 = ChirpedPulse(tau2, det2, alpha2, t0=t_start2, e0=area2, polar_x=pol2_x)
 
@@ -89,7 +101,12 @@ def biexciton_system(tau1=1, tau2=1, area1=1*np.pi, area2=0, det1=0, det2=0, alp
     H_y_dag = -0.5 * (p_gy + p_yb)
     # print(H_x_dag.dag())
 
-    H = [H_sys, [H_x_dag, lambda t,args : np.conj(pulse1.polar_x*pulse1.get_total(t) + pulse2.polar_x*pulse2.get_total(t))],
+    H_cav = g_cav * (ax.dag() * ((p_gx + p_xb) ) +  ay.dag() *(p_gy + p_yb))
+    #H_cav = g_cav * ax.dag() * p_gx + g_cav * ax.dag() * p_xb + g_cav * ay.dag() * p_gy + g_cav * ay.dag() * p_yb
+    H_cav_final = H_cav + H_cav.dag()
+
+    H = [H_sys, H_cav_final,
+                [H_x_dag, lambda t,args : np.conj(pulse1.polar_x*pulse1.get_total(t) + pulse2.polar_x*pulse2.get_total(t))],
                 [H_y_dag, lambda t,args : np.conj(pulse1.polar_y*pulse1.get_total(t) + pulse2.polar_y*pulse2.get_total(t))],
                 [H_x_dag.dag(), lambda t,args : pulse1.polar_x*pulse1.get_total(t) + pulse2.polar_x*pulse2.get_total(t)],
                 [H_y_dag.dag(), lambda t,args : pulse1.polar_y*pulse1.get_total(t) + pulse2.polar_y*pulse2.get_total(t)]]
@@ -98,6 +115,7 @@ def biexciton_system(tau1=1, tau2=1, area1=1*np.pi, area2=0, det1=0, det2=0, alp
     # two different time steps are used: a small dt_1, during the time the pulses are active, and a larger dt_2 during the decay 
     # time axis during the pulses
     t_off = np.max([2*t_start2, 2*t_start1])  # time window where pulse 1 or 2 is still active
+    rate = 2*gamma_b if 2*gamma_b<gamma_e else gamma_e
     t_end = np.max([t_off, tend])  # 1/rate *np.log(epsilon)  # note that log(epsilon) is in general negative
     t_axis1 = np.arange(0, t_off, dt_1)
     t_axis2 = np.arange(t_off, t_end, dt_2)  # note that arange does not include the final value so t_off is not in both arrays
@@ -107,59 +125,23 @@ def biexciton_system(tau1=1, tau2=1, area1=1*np.pi, area2=0, det1=0, det2=0, alp
         t_axis = np.append(t_axis1, t_exp)
 
     if mode == "pop":
-        x_occ, y_occ, b_occ, polar_gx, polar_xb, polar_gb = qt.mesolve(H, g, t_axis, c_ops=c_ops, e_ops=[n_x, n_y, n_b, p_gx, p_xb, p_gb], options=options).expect
-        return x_occ, y_occ, b_occ, polar_gx, polar_xb, polar_gb, t_axis
-    
-    elif mode == "entanglement":
+        g_occ, x_occ, y_occ, b_occ, polar_gx, polar_xb, polar_gb, nx, ny = qt.mesolve(H, initial, t_axis, c_ops=c_ops, e_ops=[n_g, n_x, n_y, n_b, p_gx, p_xb, p_gb, n_cavx, n_cavy], options=options).expect
+        return g_occ, x_occ, y_occ, b_occ, polar_gx, polar_xb, polar_gb, t_axis, nx, ny, pulse1, pulse2
+    elif mode == "g2":
         tau_axis = t_axis
 
         a_op = eval(a_op)
         b_op = eval(b_op)
         c_op = eval(c_op)
-        G2_t_tau = qt.correlation_3op_2t(H, g, t_axis, tau_axis, c_ops, a_op, b_op, c_op, solver='me',
+        G2_t_tau = qt.correlation_3op_2t(H, initial, t_axis, tau_axis, c_ops, a_op, b_op, c_op, solver='me',
                                                       options=options)
         if abs:
             G2_t_tau = np.abs(G2_t_tau)
         G2_tau = np.trapz(G2_t_tau.transpose(), t_axis)
+        # return G2_tau, tau_axis
         _g2 = np.trapz(G2_tau, tau_axis)
         return _g2, G2_t_tau, tau_axis
-    
-    elif mode == "g2":
-        tau_axis = t_axis
-        sm = p_gx
-        _n = n_x
-        if g2_pol == "y":
-            sm = p_gy
-            _n = n_y
-
-        # operators for 2-time correlations
-        # <A(t)B(t+tau)C(t)>
-        a_op = sm.dag()
-        b_op = sm.dag() * sm  # = |x><x|
-        c_op = sm
-
-        # Expectationvalue of the occupation to calculate the Brightness
-        # which we need for normalization
-        n_ex = qt.mesolve(H, g, t_axis, c_ops, e_ops=[_n], options=options).expect[0]
-        brightness_nogamma = np.trapz(n_ex, t_axis)
-        brightness = gamma_e * brightness_nogamma
-        # two-time correlation
-        G2_t_tau = gamma_e ** 2 * qt.correlation_3op_2t(H, g, t_axis, tau_axis, c_ops, a_op, b_op, c_op, solver='me',
-                                                      options=options)
-        G2_tau = np.abs(np.trapz(G2_t_tau.transpose(), t_axis))
-        g2_tau = G2_tau / brightness**2
-
-        # *2 to account for negative tau
-        g2 = 2 * np.abs(np.trapz(g2_tau, tau_axis))
-        a_op = sm.dag()
-        b_op = sm
-        # estimate for the Indistinguishability, assuming g2 is negligible. See https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.128.093603, Suppl. Material
-        G1_t_tau = qt.correlation_2op_2t(H, g, t_axis, tau_axis, c_ops, a_op, b_op, solver='me',
-                                                      options=options)
-        G1_tau = np.trapz(np.abs(G1_t_tau.transpose())**2, t_axis)
-        # *2 to account for negative tau
-        g1 = 2 * gamma_e**2 * np.trapz(G1_tau, tau_axis) / brightness**2                        
-        return brightness, g1, g2
+        
     else:
-        print("unsupported mode. choose pop, entanglement or g2")
+        print("unsupported mode. choose pop or g2")
         exit(0)
---
+++ photonprops/biexciton/pol_entanglement.py
@@ -0,0 +1,51 @@
+import numpy as np
+from qutip.parallel import parallel_map
+
+def concurrence(rho):
+    T_matrix = np.diag([-1.,1.,1.,-1.])
+    M_matrix = np.dot(rho,np.dot(T_matrix,np.dot(np.conjugate(rho),T_matrix)))
+    _eigvals = np.real(np.linalg.eigvals(M_matrix))
+    _eigvals = np.sqrt(np.sort(_eigvals))
+    return np.max([0.0,_eigvals[-1]-np.sum(_eigvals[:-1])])
+
+def calc_concurrence(system,options):
+    options = {"options": options}
+    args = ["ax.dag(),ax.dag()*ax,ax",   
+            "ay.dag(),ay.dag()*ay,ay",
+            "ax.dag(),ay.dag()*ay,ax",
+            "ay.dag(),ax.dag()*ax,ay",
+
+            "ax.dag(),ax.dag()*ay,ax",
+            "ax.dag(),ax.dag()*ax,ay",
+            "ax.dag(),ax.dag()*ay,ay",
+        
+            "ax.dag(),ay.dag()*ax,ay",
+            "ax.dag(),ay.dag()*ay,ay",
+        
+            "ay.dag(),ax.dag()*ay,ay"]
+
+    g2s = parallel_map(system,args,task_kwargs=options)
+    density_matrix = np.zeros([4,4], dtype=complex)
+    density_matrix[0,0] = g2s[0]  # xx,xx
+    density_matrix[3,3] = g2s[1]  # yy,yy
+    density_matrix[1,1] = g2s[2]  # xy,xy
+    density_matrix[2,2] = g2s[3]  # yx,yx
+    
+    density_matrix[0,1] = g2s[4]  # xx,xy
+    density_matrix[1,0] = np.conj(density_matrix[0,1])
+    density_matrix[0,2] = g2s[5]  # xx,yx
+    density_matrix[2,0] = np.conj(density_matrix[0,2])
+    density_matrix[0,3] = g2s[6]  # xx,yy
+    density_matrix[3,0] = np.conj(density_matrix[0,3])
+    
+    density_matrix[1,2] = g2s[7]  # xy,yx
+    density_matrix[2,1] = np.conj(density_matrix[1,2])
+    density_matrix[1,3] = g2s[8]  # xy,yy
+    density_matrix[3,1] = np.conj(density_matrix[1,3])
+    
+    density_matrix[2,3] = g2s[9]  # yx,yy
+    density_matrix[3,2] = np.conj(density_matrix[2,3])
+    
+    norm = np.trace(density_matrix)
+    density_matrix = density_matrix / norm
+    return concurrence(density_matrix)
